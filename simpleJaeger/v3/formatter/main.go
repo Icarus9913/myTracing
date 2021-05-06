9b245df5deacf03ec16708218bdb92d53776f65f
icaruswu <icaruswu66@qq.com>
2021-05-06 11:55:52 +0800

    RPC从client与server共用span进行追踪

---
+++ simpleJaeger/v3/formatter/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	otlog "github.com/opentracing/opentracing-go/log"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+)
+
+func Init(service string) (opentracing.Tracer, io.Closer) {
+	cfg := &config.Configuration{
+		ServiceName: service,
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
+	if err != nil {
+		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
+	}
+	return tracer, closer
+}
+
+// curl 'http://localhost:8081/format?helloTo=Icarus'
+func main() {
+	tracer, closer := Init("formatter")
+	defer closer.Close()
+
+	//这里的extract相当于接收到client端的请求,把ctx和span接收回来,这样就是从client,server端形成了一条链路进行追踪
+	http.HandleFunc("/format", func(w http.ResponseWriter, r *http.Request) {
+		spanCtx, _ := tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(r.Header))
+		span := tracer.StartSpan("format", ext.RPCServerOption(spanCtx))
+		defer span.Finish()
+
+		helloTo := r.FormValue("helloTo")
+		helloStr := fmt.Sprintf("hello, %s!", helloTo)
+		span.LogFields(
+			otlog.String("event", "string-format"),
+			otlog.String("value", helloStr),
+		)
+		w.Write([]byte(helloStr))
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
